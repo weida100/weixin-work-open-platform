4d09a71acd59a34b7b7b9fddf0644add708b55ad
Sgenmi <sgenmi@gmail.com>
2023-07-20 00:25:40 +0800

    init

---
+++ src/Application.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+/**
+ * Author: Weida
+ * Date: 2023/7/20 00:24
+ * Email: sgenmi@gmail.com
+ */
+
+namespace Weida\WeixinWorkOpenPlatform;
+
+class Application
+{
+
+}
